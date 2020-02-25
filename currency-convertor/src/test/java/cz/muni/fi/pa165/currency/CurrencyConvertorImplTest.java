dfc14f325a09168a54bf581219b95778b0ee5164
Katarina Hermanova <xherman2@nymfe34.fi.muni.cz>
2020-02-25 15:42:54 +0100

    In progress

--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -1,34 +1,57 @@
+
 package cz.muni.fi.pa165.currency;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Locale;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CurrencyConvertorImplTest {
+
+    @Mock
+    ExchangeRateTable exchangeRateTable;
+
+    private CurrencyConvertor currencyConvertor;
+    private Currency eur = Currency.getInstance(Locale.GERMANY);
+    private Currency us = Currency.getInstance(Locale.US);
+    private BigDecimal amount = new BigDecimal("1");
+
+    @BeforeClass
+    public void setUp() throws ExternalServiceFailureException {
+        currencyConvertor = new CurrencyConvertorImpl(exchangeRateTable);
+        when(exchangeRateTable.getExchangeRate(eur,us)).thenReturn(new BigDecimal(1.08));
+    }
 
     @Test
     public void testConvert() {
-        // Don't forget to test border values and proper rounding.
-        fail("Test is not implemented yet.");
+        assertEquals(new BigDecimal(1.08), currencyConvertor.convert(eur, us, amount));
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testConvertWithNullSourceCurrency() {
-        fail("Test is not implemented yet.");
+        currencyConvertor.convert(eur, us, null);
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testConvertWithNullTargetCurrency() {
-        fail("Test is not implemented yet.");
+        currencyConvertor.convert(eur, null, amount);
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testConvertWithNullSourceAmount() {
-        fail("Test is not implemented yet.");
+        currencyConvertor.convert(null, us, amount);
     }
 
-    @Test
+    @Test(expected = UnknownExchangeRateException.class)
     public void testConvertWithUnknownCurrency() {
-        fail("Test is not implemented yet.");
+        currencyConvertor.convert(Currency.getInstance("UNKNOWN"), us, amount);
     }
 
     @Test
